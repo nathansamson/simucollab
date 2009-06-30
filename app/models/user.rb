606945f0f727130ab1246d17b6a387a44f4dca28
Nathan Samson <nathansamson@gmail.com>
2009-06-30 17:11:29 +0200

    Added user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
+	validates_presence_of :name
+	validates_length_of :name, :minimum => 5
+
 	acts_as_authentic
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,73 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def test_new_user
+    user = User.new
+    user.email = 'auniquemail@mail.org'
+    user.name = 'My name'
+    user.password = 'apassword'
+    user.password_confirmation = 'apassword'
+    
+    assert user.save
+  end
+  
+  def test_new_user_incorrect_email
+    user = User.new
+    user.email = 'nomail@org'
+    user.name = 'My name'
+    user.password = 'apassword'
+    user.password_confirmation = 'apassword'
+    
+    assert !user.save
+  end
+  
+  def test_new_user_not_unique_email
+    user = User.new
+    user.email = 'anemail@email.com'
+    user.name = 'My name'
+    user.password = 'apassword'
+    user.password_confirmation = 'apassword'
+    
+    assert !user.save
+  end
+  
+  def test_new_user_no_name
+    user = User.new
+    user.email = 'google@email.com'
+    user.name = ' '
+    user.password = 'apassword'
+    user.password_confirmation = 'apassword'
+    
+    assert !user.save
+  end
+  
+  def test_new_user_passwords_do_not_match
+    user = User.new
+    user.email = 'google@email.com'
+    user.name = 'My name'
+    user.password = 'apassword'
+    user.password_confirmation = 'anotherpassword'
+    
+    assert !user.save
+  end
+  
+  def test_new_user_password_too_short
+    user = User.new
+    user.email = 'google@email.com'
+    user.name = 'My name'
+    user.password = 'apa'
+    user.password_confirmation = 'apa'
+    
+    assert !user.save
+  end
+  
+  def test_new_user_name_too_short
+    user = User.new
+    user.email = 'google@email.com'
+    user.name = 'name'
+    user.password = 'apassword'
+    user.password_confirmation = 'apassword'
+    
+    assert !user.save
   end
 end
