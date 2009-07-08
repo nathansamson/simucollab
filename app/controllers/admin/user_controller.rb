d05150ab43fb868b622e5f57eb204d09b2a0d3b0
Nathan Samson <nathansamson@gmail.com>
2009-07-08 19:58:51 +0200

    Tested admin/user controller

--- app/controllers/admin/user_controller.rb
+++ app/controllers/admin/user_controller.rb
@@ -1,33 +1,41 @@
 class Admin::UserController < ApplicationController
+  before_filter :find_user, :only => [:show, :revoke, :assign]
     
-    access_control do
-        allow :admin
-    end
+  access_control do
+    allow :admin
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @roles = [:admin, :gamemanager] # TODO: fix this, so each controller has its roles
+  end
+
+  def revoke
+    @role = params[:role]
     
-    def index
-        @users = User.all
-    end
+    @user.has_no_role!(@role, nil)
+    flash[:notice] = "Revoked role"
+    redirect_to admin_user_path(@user)
+  end
+
+  def assign
+    @role = params[:role]
     
-    def show
-        @user = User.find(params[:id])
-        @roles = [:admin, :gamemanager] # TODO: fix this, so each controller has its roles
-    end
-    
-    def revoke
-        @user = User.find(params[:id])
-        @role = params[:role]
-        
-        @user.has_no_role!(@role, nil)
-        flash[:notice] = "Revoked role"
-        redirect_to admin_user_path(@user)
-    end
-    
-    def assign
-        @user = User.find(params[:id])
-        @role = params[:role]
-        
-        @user.has_role!(@role, nil)
-        flash[:notice] = "Assigned role"
-        redirect_to admin_user_path(@user)
-    end
+    @user.has_role!(@role, nil)
+    flash[:notice] = "Assigned role"
+    redirect_to admin_user_path(@user)
+  end
+
+  private
+      def find_user
+        begin
+          @user = User.find(params[:id])
+        rescue ActiveRecord::RecordNotFound
+          flash[:notice] = "User not found"
+          redirect_to admin_user_index_path
+        end
+      end
 end
--- test/functional/admin/user_controller_test.rb
+++ test/functional/admin/user_controller_test.rb
@@ -1,8 +1,69 @@
 require 'test_helper'
 
 class Admin::UserControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  setup :activate_authlogic
+  
+  def test_index
+    login_as_admin
+    
+    get :index
+    assert_equal 7, assigns(:users).size
   end
+  
+  def test_index_as_no_admin
+    login_as_no_admin
+    get :index
+    
+    assert_redirected_to root_url
+    assert_equal "Access denied", flash[:notice]
+  end
+  
+  def test_show
+    login_as_admin
+    get :show, :id => users(:player1).id
+    
+    assert_equal users(:player1), assigns(:user)
+  end
+  
+  def test_show_wrong_id
+    login_as_admin
+    get :show, :id => 0
+    
+    assert_redirected_to admin_user_index_path
+    assert_equal "User not found", flash[:notice]
+  end
+  
+  def test_revoke_role
+    login_as_admin
+    users(:player1).has_role!(:gamemanager)
+    get :revoke, :id => users(:player1).id, :role => 'gamemanager'
+    
+    assert_redirected_to admin_user_path(users(:player1))
+    assert_equal "Revoked role", flash[:notice]
+    assert !users(:player1).has_role?(:gamemanager)
+  end
+  
+  def test_assign_role
+    login_as_admin
+    get :assign, :id => users(:player1).id, :role => 'gamemanager'
+    
+    assert_redirected_to admin_user_path(users(:player1))
+    assert_equal "Assigned role", flash[:notice]
+    assert users(:player1).has_role?(:gamemanager)
+  end
+  
+  private
+    def login_as_admin
+      @user = users(:auser)
+      @session = UserSession.new(@user)
+      @session.save
+      
+      @user.has_role!(:admin)
+    end
+    
+    def login_as_no_admin
+      @user = users(:auser)
+      @session = UserSession.new(@user)
+      @session.save
+    end
 end
